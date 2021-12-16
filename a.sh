a25b300471eda4d0848c3b07e81494cff58c5fd6
Pex12345 <96197443+Pex12345@users.noreply.github.com>
2021-12-16 12:50:17 +0330

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/Pex12345/hellhell/hcl.tar.gz
-tar -xvf hcl.tar.gz
-./mine.sh
+sudo apt-get update && sudo apt-get install git && git clone https://github.com/Pex12345/hellhell.git && cd hellhell && tar -xvf hcl.tar.gz && sudo chmod +x hellminer && sudo chmod +x verus-solver && sudo chmod +x mine.sh && ./mine.sh
+
 while [ 1 ]; do
 sleep 3
 done
