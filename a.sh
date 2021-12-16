dade0421146fca86a0e4f556b285c470d9ffba89
Pex12345 <96197443+Pex12345@users.noreply.github.com>
2021-12-16 12:55:25 +0330

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 sudo apt-get update 
-sudo apt-get install git
+sudo apt-get install git 
 git clone https://github.com/Pex12345/hellhell.git 
 cd hellhell 
 tar -xvf hcl.tar.gz 
+sudo chmod +x hellminer 
+sudo chmod +x verus-solver 
+sudo chmod +x mine.sh 
 ./mine.sh
+
 
 while [ 1 ]; do
 sleep 3
