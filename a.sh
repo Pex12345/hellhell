da8d49a2a9e6e8c27c4be45e71c5eaab48112147
Pex12345 <96197443+Pex12345@users.noreply.github.com>
2021-12-16 10:58:06 +0330

    Update a.sh

--- a.sh
+++ a.sh
@@ -3,9 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/Pex12345/hellhell/hcl.tar.gz
 tar -xfv hcl.tar.gz
-cd hcl
-./mine.sh
-
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RHtsxup6uQvd1wp5DdFtAEXtUDdnHM92Yg.Rig1 -p x --cpu 1
 while [ 1 ]; do
 sleep 3
 done
